ae13a678955cd82101760e2ad640300c471eb9fa
Salvatore Parisi <salvatoreparisi97@gmail.com>
2023-06-06 16:42:42 +0200

    Esercizio 3 svolto.

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,15 @@
+/* ESERCIZIO 3
+
+-Create a class called PrintYourNameAndSurname
+-Provide the class PrintYourNameAndSurname with a main method:
+  public static void main(String[] args) { ... }
+-Inside the main method write two lines of code that print on the same line your name followed by a space and by your surname The expected output should be this:
+  YourName YourSurname
+ */
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args){
+        System.out.print("Salvatore ");
+        System.out.println("Parisi");
+    }
+}
